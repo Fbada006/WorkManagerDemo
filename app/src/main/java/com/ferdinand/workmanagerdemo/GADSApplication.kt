ea850db9f924845ba5badd2dab761b04e372f5c2
Ferdinand Bada <33724380+Fbada006@users.noreply.github.com>
2022-11-09 14:48:26 +0300

    add periodic work

---
+++ app/src/main/java/com/ferdinand/workmanagerdemo/GADSApplication.kt
@@ -0,0 +1,38 @@
+package com.ferdinand.workmanagerdemo
+
+import android.app.Application
+import androidx.work.Constraints
+import androidx.work.ExistingPeriodicWorkPolicy
+import androidx.work.NetworkType
+import androidx.work.PeriodicWorkRequestBuilder
+import androidx.work.WorkManager
+import java.util.concurrent.TimeUnit
+
+class GADSApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        initWorkManager()
+    }
+
+    private fun initWorkManager() {
+        val constraints = Constraints.Builder().apply {
+            setRequiredNetworkType(NetworkType.CONNECTED)
+            setRequiresBatteryNotLow(true)
+        }.build()
+
+        val repeatingMovieRefreshRequest =
+            PeriodicWorkRequestBuilder<NotificationWork>(
+                15, TimeUnit.MINUTES,
+                5, TimeUnit.MINUTES
+            )
+                .setConstraints(constraints)
+                .build()
+
+        WorkManager.getInstance(applicationContext).enqueueUniquePeriodicWork(
+            NotificationWork.GADS_WORK_NAME,
+            ExistingPeriodicWorkPolicy.KEEP,
+            repeatingMovieRefreshRequest
+        )
+    }
+}
